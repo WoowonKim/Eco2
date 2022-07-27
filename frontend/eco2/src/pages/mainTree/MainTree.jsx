618d1c82b4f3c02b8661361801c53c4b08601eae
김우원 <dyffh1031@naver.com>
2022-07-27 17:57:23 +0900

    [FE] FEAT: adding leaves to main tree

--- frontend/eco2/src/pages/mainTree/MainTree.jsx
+++ frontend/eco2/src/pages/mainTree/MainTree.jsx
@@ -1,11 +1,35 @@
 import styles from "./MainTree.module.css";
+import { Leaf } from "../../components/styled";
+import { useSelector, useDispatch } from "react-redux";
+import { leavesAction } from "../../store/mainTree/leavesSlice";
 const MainTree = () => {
+  let dispatch = useDispatch();
+  let leaves = useSelector((state) => state.leaves);
+  const mouseDownHandler = (e, i, leaf) => {
+    let copy = [...leaves];
+    copy[i].left = "300px";
+    dispatch(leavesAction.changePos(copy));
+  };
   return (
     <div className={styles.Tree}>
       <img
         src={process.env.PUBLIC_URL + "tree_leaves/mainTree.png"}
         className={styles.Tree}
       ></img>
+      {leaves.map(function (leaf, i) {
+        return (
+          <Leaf
+            key={i}
+            src={process.env.PUBLIC_URL + "tree_leaves/Leaf" + leaf.id + ".png"}
+            left={leaf.left}
+            top={leaf.top}
+            onMouseDown={(e) => {
+              mouseDownHandler(e, i, leaf);
+            }}
+            //김우우너 바보멍청ㅇ;
+          ></Leaf>
+        );
+      })}
     </div>
   );
 };
