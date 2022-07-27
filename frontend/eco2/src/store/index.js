c1821927ae753f1f14aefc8d2e1cfd6c7e6806c4
김우원 <dyffh1031@naver.com>
2022-07-27 17:56:53 +0900

    [FE] STYLE: changed resorce png name

--- frontend/eco2/src/store/index.js
+++ frontend/eco2/src/store/index.js
@@ -2,11 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./user/userSlice";
 import { feedSlice } from "./mainFeed/feedSlice";
 import { commentSlice } from "./mainFeed/commentSlice";
-
+import { leavesSlice } from "./mainTree/leavesSlice";
 export const store = configureStore({
   reducer: {
     user: authSlice.reducer,
     feed: feedSlice.reducer,
     comment: commentSlice.reducer,
+    leaves: leavesSlice.reducer,
   },
 });
