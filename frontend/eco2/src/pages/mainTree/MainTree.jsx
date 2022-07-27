fd9de9a25bbe1fd225b96e2af0e0538cb0e9ab4a
김우원 <dyffh1031@naver.com>
2022-07-27 11:35:38 +0900

    [FE] REFACTOR: App.js css and polled out header and nav out of app<div>

--- frontend/eco2/src/pages/mainTree/MainTree.jsx
+++ frontend/eco2/src/pages/mainTree/MainTree.jsx
@@ -1,7 +1,12 @@
 import styles from "./MainTree.module.css";
 const MainTree = () => {
   return (
-    <div className={styles.Tree}>메인트리입니다. 이제 나무가 생길거에요</div>
+    <div className={styles.Tree}>
+      <img
+        src={process.env.PUBLIC_URL + "tree_leaves/mainTree.png"}
+        className={styles.Tree}
+      ></img>
+    </div>
   );
 };
 
