349f5138b460042c8947732068171ec6a0cf32e1
WoowonKim <dyffh1031@gmail.com>
2022-07-29 09:08:04 +0900

    [FE] CHORE: add dependency, react-dnd, immutabillty-helper

--- frontend/eco2/src/store/mainTree/leavesSlice.js
+++ frontend/eco2/src/store/mainTree/leavesSlice.js
@@ -3,37 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 export let leavesSlice = createSlice({
   name: "leaves",
   initialState: [
-    { id: 1, left: "30px", top: "300px" },
-    {
-      id: 2,
-      left: "40px",
-      top: "300px",
-    },
-    {
-      id: 3,
-      left: "300px",
-      top: "300px",
-    },
-    {
-      id: 4,
-      left: "60px",
-      top: "200px",
-    },
-    {
-      id: 5,
-      left: "150px",
-      top: "300px",
-    },
-    {
-      id: 6,
-      left: "80px",
-      top: "300px",
-    },
+    { id: 1, left: 400, top: 300, category: 1 },
+    { id: 2, left: 420, top: 300, category: 2 },
+    { id: 3, left: 440, top: 300, category: 3 },
+    { id: 4, left: 460, top: 300, category: 4 },
+    { id: 5, left: 480, top: 300, category: 5 },
+    { id: 6, left: 500, top: 300, category: 6 },
   ],
   reducers: {
     changePos(state, action) {
-      console.log(action.payload);
-      state = action.payload;
+      let leaf = action.payload;
+      let index = state.findIndex((x) => x.id == leaf.id);
+      state.splice(index, 1);
+      state.push(leaf);
     },
   },
 });
