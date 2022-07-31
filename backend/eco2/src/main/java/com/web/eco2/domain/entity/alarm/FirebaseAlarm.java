006cd8a6936a7571da638649c333bdb4d92be0be
smj <smjeong53@gmail.com>
2022-07-31 13:16:13 +0900

    [BE] FEAT: add friend request/accept notification

---
+++ backend/eco2/src/main/java/com/web/eco2/domain/entity/alarm/FirebaseAlarm.java
@@ -0,0 +1,39 @@
+package com.web.eco2.domain.entity.alarm;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@ToString
+@Data
+public class FirebaseAlarm {
+    private String id;
+    private Long userId; // 받는 사람
+    private LocalDateTime sendTime = LocalDateTime.now();
+    private String content;
+    private String url;
+    private String dType;
+    private Long senderId; // 보낸 사람
+
+    //user 2개
+//    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    @JoinColumn(name = "usr_id", nullable = false)
+//    private User user;
+//
+//    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    @JoinColumn(name = "usr_id", nullable = false)
+//    private User user1;
+
+    @Builder
+    public FirebaseAlarm(String id, Long userId, String content, String url, String dType, Long senderId) {
+        this.id = id;
+        this.userId = userId;
+        this.content = content;
+        this.url = url;
+        this.dType = dType;
+        this.senderId = senderId;
+        this.sendTime = LocalDateTime.now();
+    }
+}
