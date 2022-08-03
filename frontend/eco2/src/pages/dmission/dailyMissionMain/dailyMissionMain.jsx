b3477ece02c20b5220fae7160b6d5889d7393818
Shin Young Jei <zerojei0423@naver.com>
2022-08-03 10:11:36 +0900

    [FE] FEAT: DailyMission Clear

--- frontend/eco2/src/pages/dmission/dailyMissionMain/dailyMissionMain.jsx
+++ frontend/eco2/src/pages/dmission/dailyMissionMain/dailyMissionMain.jsx
@@ -4,8 +4,41 @@ import styles from "./dailyMission.module.css";
 import { GreenBtn } from "../../../components/styled";
 import { useSelector } from "react-redux";
 import MissionMain from "../../../components/dailyMission/missionList/missionMain";
+import { useNavigate } from "react-router-dom";
+import MissionModal from "../../../components/dailyMission/missionClear/missionModal";
+
 const DailyMissionMain = () => {
-  const test = useSelector((state) => state.missionMain.ecoMissionList);
+  const ecoMainList = useSelector((state) => state.missionMain.ecoMissionList);
+  const ecoLength = ecoMainList.length;
+  const [tCnt, setTCnt] = useState(0);
+  const naviGate = useNavigate();
+
+  const trashCnt = (trash) => {
+    if (!trash) {
+      setTCnt(tCnt + 1);
+    } else {
+      setTCnt(tCnt - 1);
+    }
+  };
+
+  const onSucsses = () => {
+    if (tCnt === ecoLength) {
+      alert("나뭇잎 획득 메인페이지로 이동합니다.");
+      naviGate("/mainFeed");
+    } else {
+      alert("미션을 완료하고 눌러주세요!");
+    }
+  };
+
+  // const [modalOpen, setModalOpen] = useState(false);
+
+  // const openModal = () => {
+  //   setModalOpen(true);
+  // };
+  // const closeModal = () => {
+  //   setModalOpen(false);
+  // };
+
   return (
     <div className={styles.headerMain}>
       <div className={styles.mainHeader}>
@@ -17,13 +50,19 @@ const DailyMissionMain = () => {
           </span>
         </Link>
       </div>
+      {/* <div>
+        <button onClick={openModal}>모달 TEST</button>
+        <MissionModal open={modalOpen} close={closeModal} header="미션 완료">
+          인증글 작성하러가기!
+        </MissionModal>
+      </div> */}
       <div>
-        {test.map((it) => (
-          <MissionMain key={it.id} content={it.content} id={it.id} />
+        {ecoMainList.map((it) => (
+          <MissionMain key={it.id} content={it.content} id={it.id} trashCnt={trashCnt} />
         ))}
       </div>
       <div className={styles.btn}>
-        <GreenBtn>오늘 미션 보상 받기</GreenBtn>
+        <GreenBtn onClick={onSucsses}>오늘 미션 보상 받기</GreenBtn>
       </div>
     </div>
   );
