43b7007790d34ce1a24f3715874da433c3645cd2
Shin Young Jei <zerojei0423@naver.com>
2022-08-05 17:29:35 +0900

    DUMMY COMMIT

--- frontend/eco2/src/store/axiosService.js
+++ frontend/eco2/src/store/axiosService.js
@@ -4,7 +4,7 @@ import { getToken } from "./user/common";
 export const axiosService = axios.create({
   baseURL: process.env.REACT_APP_BE_HOST,
   headers: {
-    "Content-Type": "application/json",
+    // "Content-Type": "application/json",
     "Auth-accessToken": getToken(),
   },
 });
