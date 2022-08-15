51c1f36bd6fcf7628af9800f9e20cdd4f313e6b4
rlamsp234 <rlamsp234@naver.com>
2022-08-15 17:02:36 +0900

    [BE] FIX: Post Lookup Fix

--- backend/eco2/src/main/java/com/web/eco2/model/repository/FriendRepository.java
+++ backend/eco2/src/main/java/com/web/eco2/model/repository/FriendRepository.java
@@ -3,8 +3,6 @@ package com.web.eco2.model.repository;
 import com.web.eco2.domain.entity.Friend;
 import com.web.eco2.domain.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
