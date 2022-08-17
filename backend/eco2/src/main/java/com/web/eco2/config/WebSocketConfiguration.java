54836b4e6eb14b124640e40b7d7aacaac9ff0e45
rlamsp234 <rlamsp234@naver.com>
2022-08-17 16:33:04 +0900

    [BE] Code Cleaning
    Former-commit-id: 96ea9a28d74335dfed1c44e4b8f6919b42e0c2a9

--- backend/eco2/src/main/java/com/web/eco2/config/WebSocketConfiguration.java
+++ backend/eco2/src/main/java/com/web/eco2/config/WebSocketConfiguration.java
@@ -1,6 +1,5 @@
 package com.web.eco2.config;
 
-import com.web.eco2.util.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -13,8 +12,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-    private final StompHandler stompHandler;
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
@@ -26,12 +23,8 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Client 에서 SEND 요청을 처리
         registry.setApplicationDestinationPrefixes("/pub");
-        //해당 경로로 SimpleBroker를 등록.
-        // SimpleBroker는 해당하는 경로를 SUBSCRIBE하는 Client에게 메세지를 전달하는 간단한 작업을 수행
         registry.enableSimpleBroker("/sub");
-        //enableStompBrokerRelay
-        //SimpleBroker의 기능과 외부 Message Broker( RabbitMQ, ActiveMQ 등 )에 메세지를 전달하는 기능을 가짐
+
     }
 }
